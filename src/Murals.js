2c401641830c8c0457b0906a8642af4fea973b53
JohnMDoll <johnmdoll3@gmail.com>
2023-04-07 11:14:03 -0500

    initial commit with base project created

---
+++ src/Murals.js
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom"
+// import { Authorized } from "./views/Authorized"
+// import { ApplicationViews } from "./views/ApplicationViews"
+
+
+export const Murals = () => {
+	return (
+      <div>
+      Poop <br/>
+      For real though <br/>
+      Please make modules to nav to
+      </div>
+      //All the different pages we want to route to go here ->
+    // <Routes>
+    //   {/* <Route path="/" element={<Landing />} />
+    //   <Route path="/home" element={<Landing />} />
+    //   <Route path="/register" element={<Register />} /> */}
+    //   <Route
+    //     path="*"
+    //     element={
+    //       // <Authorized>
+    //         <>
+    //           {/* <ApplicationViews /> */}
+    //         </>
+    //       // </Authorized>
+    //     }
+    //   />
+    // </Routes>
+  );
+}
