815deb6384ecd63e64dcf085d3b0e7c858a90085
Matthew Hedges <matthedges35@gmail.com>
2023-04-09 18:33:33 -0500

    AppViews issues needs to be fixed

--- src/Murals.js
+++ src/Murals.js
@@ -2,29 +2,26 @@ import { Route, Routes } from "react-router-dom"
 import { getHoods } from "./components/managers/hoods_manager";
 import { getMurals } from "./components/managers/murals_manager";
 import { getSavedMurals } from "./components/managers/savedmurals_manager";
-// import { Authorized } from "./views/Authorized"
-// import { ApplicationViews } from "./views/ApplicationViews"
+import { Authorized } from "./components/views/Authorized";
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register";
+import { ApplicationViews } from "./components/views/ApplicationViews";
+
 
 
 export const Murals = () => {
-	return (
-      <div>
-      </div>
-      //All the different pages we want to route to go here ->
-    // <Routes>
-    //   {/* <Route path="/" element={<Landing />} />
-    //   <Route path="/home" element={<Landing />} />
-    //   <Route path="/register" element={<Register />} /> */}
-    //   <Route
-    //     path="*"
-    //     element={
-    //       // <Authorized>
-    //         <>
-    //           {/* <ApplicationViews /> */}
-    //         </>
-    //       // </Authorized>
-    //     }
-    //   />
-    // </Routes>
-  );
+	return <Routes>
+		<Route path="/login" element={<Login />} />
+		<Route path="/register" element={<Register />} />
+
+		<Route path="*" element={
+			<Authorized>
+				<>
+					{/* <NavBar /> */}
+					<ApplicationViews />
+				</>
+			</Authorized>
+
+		} />
+	</Routes>
 }
