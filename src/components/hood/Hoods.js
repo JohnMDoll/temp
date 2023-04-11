0bcab6bc6cfa2069d5f248940d7c7641d418b12d
kimberly burton <kj.burton03@gmail.com>
2023-04-11 11:10:57 -0500

    I love commitment

---
+++ src/components/hood/Hoods.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./Hood.css"
+
+export const Neighborhood = (props) => {
+    const navigate = useNavigate()
+    return (
+        <>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+            <h1>hi  </h1>
+
+        </>
+    )
+}
