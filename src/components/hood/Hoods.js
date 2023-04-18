732552641808944355088f2022d2fd5b092391f3
kimberly burton <kj.burton03@gmail.com>
2023-04-18 12:23:03 -0500

    connected hoods & murals to their pages

--- src/components/hood/Hoods.js
+++ src/components/hood/Hoods.js
@@ -1,14 +1,34 @@
-import React, { useEffect, useState } from "react"
-// import "./Hood.css"
-import { getHoods } from "../managers/hoods_manager"
-import { getMurals } from "../managers/murals_manager"
+import React, { useEffect, useState} from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { getHoods } from "../managers/hoods_manager";
 
-export const Hood = () => {
-    return (
-        <div>
-        <h1> hello </h1>
+export const HoodsList = (props) => {
+    const navigate = useNavigate()
+
+    const [ hoods, setHoods ] = useState([])
         
+    useEffect(() => {
+        getHoods().then(data => setHoods(data))
+    }, [])
 
-        </div>
+    return (
+        <body>
+        <h1>Neighborhoods</h1>
+        <article className="hoods">
+            {
+                hoods.map(hood => {
+                    return <section key={`mural--${hood.id}`} className="event">
+                        <header className="mural__name"> {hood.name}</header>
+                        {/* <div className="mural__location"> Location: {.address} </div> */}
+                        {/* <div className="mural__img">{mural.img}</div> */}
+                        {/* <div className="event__type">Event Type:{event?.eventType?.eventType} </div> */}
+                    </section>
+                })
+            }
+        </article>
+        </body>
     )
 }
+// go to an individual Hood, 
+// share its descirption and murals 
+// murals have links to mural info
