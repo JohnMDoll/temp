1548b105e5775915827e05b8a8bd860d266efd9f
John Doll <johnmdoll3@gmail.com>
2023-04-28 12:30:07 -0500

    removed extra mural and hoods header, clicked hood from open open by default on click from home page hood slide

--- src/components/hood/HoodMurals.js
+++ src/components/hood/HoodMurals.js
@@ -7,7 +7,7 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import PlaceHolderImage from "../../assets/MURALS.png"
 
 
-export const HoodMurals = ({ hood_id, hood_name }) => {
+export const HoodMurals = ({ hood_id, hood_name, defaultOpen }) => {
     const [murals, setMurals] = useState([])
 
     useEffect(() => {
@@ -36,7 +36,7 @@ export const HoodMurals = ({ hood_id, hood_name }) => {
     }
 
     return <>
-        <Collapsible className="murals_collapse" triggerOpenedClassName="murals_collapse" trigger={`${hood_name} Murals`}>
+        <Collapsible className="murals_collapse" triggerOpenedClassName="murals_collapse" open={defaultOpen} trigger={`${hood_name} Murals`}>
             <section className="hood__cards">
 
                 {
